cc9f94a67bc4afa4a83a3be8a384e64b1b768e9a
Shreyas-K3 <shreyasnitinpro@gmail.com>
2025-11-28 19:26:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def iso_to_readable(iso_str):
+    try:
+        return iso_str.replace("T", " ").split(".")[0]
+    except:
+        return iso_str
